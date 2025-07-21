4de5fb3a4199e3d500394886b16d63d13e5b69b2
shyytsm <139909980+shyytsm@users.noreply.github.com>
2025-07-22 00:36:27 +0200

    clean-my-pc.sh

--- clean-my-pc.sh
+++ clean-my-pc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo " Nettoyage des fichiers temporaires..."
+echo " Installation de clean-my-pc..."
 
-rm -rf /tmp/*
-rm -rf ~/.cache/*
+# info > Copier le script dans /usr/local/bin pour le rendre accessible globalement
+sudo cp clean-my-pc.sh /usr/local/bin/clean-my-pc
+sudo chmod +x /usr/local/bin/clean-my-pc
 
-echo " Nettoyage terminé."
+echo " Script installé avec succès."
+
+# info> Lancer immédiatement le script après l'installation
+echo " Lancement immédiat de clean-my-pc..."
+clean-my-pc
