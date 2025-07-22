7a658107ffda25c17f911f35ac606da70f3c199a
shyytsm <139909980+shyytsm@users.noreply.github.com>
2025-07-22 19:04:48 +0200

    Update clean-my-pc.sh

--- clean-my-pc.sh
+++ clean-my-pc.sh
@@ -2,12 +2,10 @@
 
 echo " Installation de clean-my-pc..."
 
-# info > Copier le script dans /usr/local/bin pour le rendre accessible globalement
 sudo cp clean-my-pc.sh /usr/local/bin/clean-my-pc
 sudo chmod +x /usr/local/bin/clean-my-pc
 
 echo " Script installé avec succès."
 
-# info> Lancer immédiatement le script après l'installation
 echo " Lancement immédiat de clean-my-pc..."
 clean-my-pc
